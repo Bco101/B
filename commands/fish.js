e0fe1775714b466f66fd24d930bf0cfc2cfe9b72
manybaht <nantapon.sookpeamron@gmail.com>
2020-08-11 20:11:18 +0700

    more fish more random more madness shit

--- commands/fish.js
+++ commands/fish.js
@@ -25,19 +25,71 @@ exports.run = async (client, message, args) => {
         .setDescription(`คุณเพิ่งตกปลาไป\n\nลองใหม่อีกครั้งใน ${time.minutes} นาที ${time.seconds} วิ`);
         message.channel.send(timeEmbed)
       } else {
-
-        let replies = ['<:1_:742413101265059911>','<:2_:742414967919345726>','<:3_:742414968015683726>','<:4_:742414968867258411>','<:5_:742414967957094412>','<:6_:742414968590172320>']
-
+		 
+		function getNumber(){
+        return (getNumber.number = Math.floor(Math.random() * 3) + 1) === getNumber.lastNumber ? getNumber() : getNumber.lastNumber = getNumber.number;
+        }
+		
+		let moreitems = getNumber();
+		
+		let replies = ['<:1_:742413101265059911>','<:2_:742414967919345726>','<:3_:742414968015683726>','<:4_:742414968867258411>','<:5_:742414967957094412>','<:6_:742414968590172320>']
+		
+		if (moreitems == 1) {
+		
         let result = Math.floor((Math.random() * replies.length));
-        let amount = Math.floor(Math.random() * 3) + 1;
+        let amount = getNumber();
+		
+		await db.add(`inv_space_${user.id}`, amount)
+        await db.add(`inv_${replies[result]}_${user.id}`, amount)
+        await db.set(`fish_${user.id}`, Date.now())
+		
         let embed1 = new Discord.MessageEmbed()
         .setColor("#ffb73b")
-        .setDescription(`คุณตกปลาได้ ${replies[result]} จำนวน ${amount} ตัว`);
-        message.channel.send(embed1)
-        
-		db.add(`inv_space_${user.id}`, amount)
-        db.add(`inv_${replies[result]}_${user.id}`, amount)
-        db.set(`fish_${user.id}`, Date.now())
+        .addField("คุณตกปลาได้", `${replies[result]} จำนวน ${amount} ตัว`);
+		return message.channel.send(embed1)
+		
+		} else if (moreitems == 2) {
+			
+		let result = Math.floor((Math.random() * replies.length));
+		let result2 = Math.floor((Math.random() * replies.length));
+        let amount = getNumber();
+		let amount2 = getNumber();
+		
+		await db.add(`inv_space_${user.id}`, amount)
+		await db.add(`inv_space_${user.id}`, amount2)
+        await db.add(`inv_${replies[result]}_${user.id}`, amount)
+		await db.add(`inv_${replies[result2]}_${user.id}`, amount2)
+        await db.set(`fish_${user.id}`, Date.now())
+		
+        let embed2 = new Discord.MessageEmbed()
+        .setColor("#ffb73b")
+        .addField("คุณตกปลาได้", `${replies[result]} จำนวน ${amount} ตัว\n${replies[result2]} จำนวน ${amount2} ตัว`);
+		return message.channel.send(embed2)
+		
+		} else if (moreitems == 3) {
+			
+		let result = Math.floor((Math.random() * replies.length));
+		let result2 = Math.floor((Math.random() * replies.length));
+		let result3 = Math.floor((Math.random() * replies.length));
+        let amount = getNumber();
+		let amount2 = getNumber();
+		let amount3 = Math.floor(Math.random() * 3) + 1;
+		
+		await db.add(`inv_space_${user.id}`, amount)
+		await db.add(`inv_space_${user.id}`, amount2)
+		await db.add(`inv_space_${user.id}`, amount3)
+        await db.add(`inv_${replies[result]}_${user.id}`, amount)
+		await db.add(`inv_${replies[result2]}_${user.id}`, amount2)
+		await db.add(`inv_${replies[result3]}_${user.id}`, amount3)
+        await db.set(`fish_${user.id}`, Date.now())
+		
+        let embed2 = new Discord.MessageEmbed()
+        .setColor("#ffb73b")
+        .addField("คุณตกปลาได้", `${replies[result]} จำนวน ${amount} ตัว\n${replies[result2]} จำนวน ${amount2} ตัว\n${replies[result3]} จำนวน ${amount3} ตัว`);
+		return message.channel.send(embed2)
+		
+		}
+		
     };
 		
 }
