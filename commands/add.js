19ed6b02f44e3b786894bb94f8f82cced11944db
manybaht <nantapon.sookpeamron@gmail.com>
2020-08-12 13:04:31 +0700

    Revision add.js

--- commands/add.js
+++ commands/add.js
@@ -7,15 +7,30 @@ exports.run = async (client, message, args) => {
 	    return message.channel.send("เฉพาะเจ้าของบอทเท่านั้นที่ใช้คำสั่งนี้ได้");
 	}
 	
-    let user = message.mentions.members.first() || message.author;
-	  
-	if (isNaN(args[1])) return;
+    let user = message.mentions.members.first();
+	
+	let addamount = parseInt(args[1])
+	
+	if (!user) {
+	let nousermention = new Discord.MessageEmbed()
+    .setColor(0xffb73b)
+    .setDescription("กรุณาระบุคนที่ต้องการเพิ่ม\n\nตัวอย่าง .add @ผู้ใช้ จำนวนเงิน");
+    return message.channel.send(nousermention)
+	}
+	
+	if (!addamount) {
+	let noamount = new Discord.MessageEmbed()
+    .setColor(0xffb73b)
+    .setDescription("กรุณาระบุจำนวนเงิน\n\nตัวอย่าง .add @ผู้ใช้ จำนวนเงิน");
+    return message.channel.send(noamount)
+	}
+	
     db.add(`money_${user.id}`, args[1])
     let bal = await db.fetch(`money_${user.id}`)
 	  
-	let moneyEmbed = new Discord.MessageEmbed()
+	let SendMoney = new Discord.MessageEmbed()
     .setColor(0xffb73b)
     .setDescription(`เพิ่มเงินจำนวน ${args[1]} เรียบร้อยแล้ว\n\nจำนวนเงินใหม่ : ${bal}`);
-    message.channel.send(moneyEmbed)
+    message.channel.send(SendMoney)
 	  
 }
