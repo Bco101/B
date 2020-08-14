6bc15f62c3f85d4928992f0dddbada4c9bc71e66
manybaht <nantapon.sookpeamron@gmail.com>
2020-08-14 20:01:31 +0700

    Add Reaction Role Commands

--- commands/help.js
+++ commands/help.js
@@ -4,12 +4,12 @@ exports.run = async (client, message, args) => {
 
     let bothelp = new Discord.MessageEmbed()
     .setColor("#ffb73b")
-    .addField("คำสั่งทั่วไป", "`.avatar` `.dm` `.help` `.info` `.nick` `.say` `.getverify` `.verify` `.introduce` `.introduce-info`")
+    .addField("คำสั่งทั่วไป", "`.avatar` `.dm` `.help` `.info` `.nick` `.say` `.getverify` `.verify` `.introduce` `.introduce-info` `.react-info`")
     .addField("คำโปรไฟล์", "`.profile` `.setnickname` `.setgender` `.setstatus` `.inv`")
     .addField("คำสั่งสนุก ๆ", "`.bet` `.slap` `.smug` `.pat` `.unknow`")
     .addField("คำสั่งเงิน", "`.bal` `.work` `.deposit` `.withdraw` `.pay` `.rob` `.fish` `.mine` `.sell` `.roulette`")
     .addField("คำสั่งอื่น ๆ", "`.minesend` `.fivem-info`")
-    .addField("คำสั่งแอดมิน", "`.ban` `.kick` `.purge` `.verify-admin` `.verify-role` `.question-add` `.question-admin` `.question-log` `.question-reset` `.question-role`");
+    .addField("คำสั่งแอดมิน", "`.ban` `.kick` `.purge` `.verify-admin` `.verify-role` `.question-add` `.question-admin` `.question-log` `.question-reset` `.question-role` `.react-add` `.react-reset` `.react-close` `.react-open`");
     message.channel.send(bothelp)
 	
 }
